a66b94a6446d51ff8db9195eef26ce2f5a58ed67
ThomasPoum <thomas.poumarede@gmail.com>
2023-06-22 10:39:47 +0200

    escape

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -110,7 +110,7 @@ export function Pricing() {
       adaptée à chaque besoin.
     </h2>
     <p className="mt-4 text-lg text-slate-400">
-      De la TPE à l'entreprise établie, nous avons l'offre qui vous correspond.
+      De la TPE à l’entreprise établie, nous avons l’offre qui vous correspond.
     </p>
   </div>
   <div className="-mx-4 mt-16 grid max-w-2xl grid-cols-1 gap-y-10 sm:mx-auto lg:-mx-8 lg:max-w-none lg:grid-cols-3 xl:mx-0 xl:gap-x-8">
