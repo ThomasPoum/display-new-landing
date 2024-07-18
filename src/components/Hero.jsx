c14f49c914dbd897c33c7e552bfa5f6d9a6da334
ThomasPoum <thomas.poumarede@gmail.com>
2024-07-18 20:56:03 +0200

    remove button vers clip musique

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,7 +49,7 @@ export function Hero() {
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
         <Button href="/register">Réserver une démo</Button>
-        <Button
+        {/* <Button
           href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           variant="outline"
           target="_blank"
@@ -61,7 +61,7 @@ export function Hero() {
             <path d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z" />
           </svg>
           <span className="ml-3">Voir un réseau optimisé en 30min</span>
-        </Button>
+        </Button> */}
       </div>
 
       <div 
