9e424a4ba4a20d0f26a68dea4c91dde1a3dd92a7
ThomasPoum <thomas.poumarede@gmail.com>
2024-07-18 20:48:10 +0200

    fix analytics and speedinsights

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,10 +4,12 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
-  return 
-  <>
+  return (
+    <>
     <Component {...pageProps} />
-    <Analytics />
+    <Analytics/>
     <SpeedInsights/>
-  </>
+    </>
+  
+)
 }
